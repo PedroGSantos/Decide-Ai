c9df7a6d7068d714244ac1a2244ab7f2ff36d925
PedroGSantos <pedrogsantos_10@hotmail.com>
2024-07-18 16:46:49 -0300

    fix

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,39 +1,34 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import './style.css'
-
-//k_hg4pap4f
-//k_s7ar6knl
+import "./style.css";
 
 export function Login() {
 	const navigate = useNavigate();
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 	const [errorLogin, setErrorLogin] = useState(false);
 
-
-	async function login(){
-	const url = `http://localhost:3333/users`;
-	fetch(url)
-      .then(response => response.json())
-      .then(data => {
-		
-        const user = data.find(user => user.email === email);
-		console.log(user)
-		if(user === undefined){
-			console.log("Não encontrou usuário com esse e-mail")
-			setErrorLogin(true);
-			return
-		}else if(password === user.password){
-			localStorage.setItem('email', user.email)
-			localStorage.setItem('id', user.user_id)
-			navigate("/filmesAssistidos")
-		}else{
-			setErrorLogin(true);
-		}
-
-      })
-      .catch(error => console.error(error));
+	async function login() {
+		console.log(process.env.REACT_APP_API_URL);
+		const url = `${process.env.REACT_APP_API_URL}/users`;
+		fetch(url)
+			.then((response) => response.json())
+			.then((data) => {
+				const user = data.find((user) => user.email === email);
+				console.log(user);
+				if (user === undefined) {
+					console.log("Não encontrou usuário com esse e-mail");
+					setErrorLogin(true);
+					return;
+				} else if (password === user.password) {
+					localStorage.setItem("email", user.email);
+					localStorage.setItem("id", user.user_id);
+					navigate("/filmesAssistidos");
+				} else {
+					setErrorLogin(true);
+				}
+			})
+			.catch((error) => console.error(error));
 	}
 
 	const handleChangeEmail = (event) => {
@@ -42,30 +37,36 @@ export function Login() {
 
 	const handleChangePassword = (event) => {
 		setPassword(event.target.value);
-	  };
+	};
 
 	return (
 		<div className="box">
-			<div className="tituloLogin">  
+			<div className="tituloLogin">
 				<p>DECIDE AÍ</p>
 			</div>
 
 			<div className="loginBox">
 				<div className="loginContainer">
-					{
-						errorLogin === true? 
-						<p>E-mail ou senha incorretos</p>:
+					{errorLogin === true ? (
+						<p>E-mail ou senha incorretos</p>
+					) : (
 						<p></p>
-					}
+					)}
 					<h1>Entrar</h1>
 					<div className="inputsContainer">
-						<input placeholder="E-mail" onChange={handleChangeEmail}></input>
-						<input placeholder="Senha" type={"password"} onChange={handleChangePassword}></input>
+						<input
+							placeholder="E-mail"
+							onChange={handleChangeEmail}
+						></input>
+						<input
+							placeholder="Senha"
+							type={"password"}
+							onChange={handleChangePassword}
+						></input>
 						<button onClick={() => login()}>ENTRAR</button>
 					</div>
 				</div>
 			</div>
 		</div>
-
-	)
+	);
 }
